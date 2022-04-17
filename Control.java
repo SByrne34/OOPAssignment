1ef2b721c2f168353cd3ba0f9cce5b793aa135f3
SByrne34 <102997923+SByrne34@users.noreply.github.com>
2022-04-17 13:53:03 +0100

    Update Control.java

--- Control.java
+++ Control.java
@@ -1,9 +1,12 @@
+// Name: Stephen Byrne
+
 package assignment;
 
 public class Control
 {
 	public static void main (String [] args)
 	{
+		// Run FileGUI class 
 		FileGUI GUIControl = new FileGUI("The File GUI");
 	}	
 }
