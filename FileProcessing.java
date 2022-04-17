6d275aa2c9a5aa5c46a056fccef15ee98acbb7cc
SByrne34 <102997923+SByrne34@users.noreply.github.com>
2022-04-17 01:00:07 +0100

    Update FileProcessing.java

--- FileProcessing.java
+++ FileProcessing.java
@@ -40,10 +40,66 @@ public class FileProcessing {
 				stopList.add(Scan1.next());
 			}
 			Scan1.close();
-    }
     
-    catch (FileNotFoundException e) 
+			File newFile = new File(Filename);
+			Scan2 = new Scanner(newFile);
+			
+			while (Scan2.hasNext())
+			{
+				String word = Scan2.next();
+				if (stopList.contains(word))
+				{
+					
+				}
+				else
+				{
+					Array1.add(word);
+				}
+			}
+			Scan2.close();
+			
+			
+			for (String Words : Array1)
+			{
+				fileMap.put(Words, Collections.frequency(Array1, Words));
+			}
+			
+
+			// Taken from online source: https://stackoverflow.com/questions/43922882/how-to-print-hashmap-elements-from-max-to-min
+			fileMap.entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByValue().reversed()).limit(n)
+            
+            .forEach(x ->
+                    {
+                        System.out.println(x.getKey() + " Amount = "+ x.getValue());
+                    });
+			
+		
+			for (String element : fileMap.keySet())
+			{
+				if (Temporary.containsKey(element)) 
+                { 
+						
+			
+                  if (Temporary.containsValue(fileMap.get(element))) 
+                  { 
+                      tally++;
+                  }
+                }
+			}
+			
+	
+			if (k == 0)
+			{
+
+				Temporary.putAll(fileMap);
+				k = 1;
+			}
+		}
+        
+		catch (FileNotFoundException e) 
 		{
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 		}
+	}
+}
